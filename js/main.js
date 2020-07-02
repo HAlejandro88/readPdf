4c583f0cbf8efa0c7e5f19fc2f820e14f3c779ab
Alejandro Soto Garcia <alejandro.soto@iMac-de-web.local>
2020-07-02 14:35:06 -0500

    se sube visor de pdf js Vanilla

--- js/main.js
+++ js/main.js
@@ -21,8 +21,57 @@ const renderPage = num => {
     pdfDoc.getPage(num).then(page => {
         // Set Scale
         const viewport =  page.getViewPort({scale })
+        canvas.height = viewport.height;
+        canvas.width = viewport.width;
+
+        const renderCtx = {
+            canvasContext: ctx,
+            viewport
+        }
+
+        page.render(renderCtx).promise.then(() => {
+            pageIsRendering = false;
+
+            if (pageNumIsPending !== null) {
+                renderPage(pageNumIsPending);
+                pageNumIsPending = null;
+
+            }
+        })
+
+        // Output current page
+        document.querySelector('#page-num').textContent = num;
     })
 }
+
+// check for pages rendering
+const queueRenderPage = num => {
+    if (pageIsRendering) {
+        pageNumIsPending = num;
+    } else {
+        renderPage(num)
+    }
+}
+
+// Show Previus Page
+ const showPrevPage = () => {
+     if (pageNum <= 1) {
+        return;
+     }
+     pageNum--;
+     queueRenderPage(pageNum);
+ }
+
+
+ // Show Next Page
+
+ const showNextPage = () => {
+    if (pageNum >= pdfDoc.numPages) {
+        return;
+     }
+     pageNum++;
+     queueRenderPage(pageNum);
+ }
 
 // Get the Document
 pdfjsLib.getDocument(url).promise.then(pdfDoc_ =>  {
@@ -32,3 +81,17 @@ pdfjsLib.getDocument(url).promise.then(pdfDoc_ =>  {
 
     renderPage(pageNum);
 })
+.catch(err => {
+    // Display error
+    const div = document.createElement('div');
+    div.className = 'error';
+    div.appendChild(document.createTextNode(err.messge));
+    document.querySelector('body').insertBefore(div, canvas);
+
+    // remove top bar
+    document.querySelector('.top-bar').style.dispay = 'none';
+})
+
+// Button Events
+document.querySelector('#prev-page').addEventListener('click', showPrevPage);
+document.querySelector('#next-page').addEventListener('click', showNextPage);
